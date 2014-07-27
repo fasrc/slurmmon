170d0a38b6833baad6dfe36c58b829e7b8fec415
John Brunelle <john_brunelle@harvard.edu>
2014-07-27 11:45:38 -0400

    misc whitespace and hex code consistency w/ other files

--- var/www/ganglia/graph.d/slurm_probejob_pendtime.php
+++ var/www/ganglia/graph.d/slurm_probejob_pendtime.php
@@ -1,34 +1,29 @@
 <?php
-
-
 function graph_slurm_probejob_pendtime ( &$rrdtool_graph ) {
-        global $rrd_dir;
+	global $rrd_dir;
 
-        $config = file_get_contents("/etc/slurmmon.conf");
-        $json_conf= json_decode($config,true);
+	$config = file_get_contents("/etc/slurmmon.conf");
+	$json_conf = json_decode($config,true);
 
-        $series="";
-        foreach ( $json_conf['probejob_partitions'] as $partition)
-        {
-                $series =  $series."DEF:'".$partition."'='${rrd_dir}/slurmmond_probejob_pendtime_".$partition.".rrd':'sum':AVERAGE ";
-                                
-        }
+	$series = "";
+	foreach ($json_conf['probejob_partitions'] as $partition)
+	{
+		$series = $series."DEF:'".$partition."'='${rrd_dir}/slurmmond_probejob_pendtime_".$partition.".rrd':'sum':AVERAGE ";
+	}
 
-        $colours=array("#00000f","#FF0000","#00FF00","#FFFF00","#0000FF");
-        $i=0;
-        foreach ( $json_conf['probejob_partitions'] as $partition)
-        {
-                $series =  $series."LINE2:'".$partition."'".$colours[$i].":'".$partition."' ";
-                $i++;
-                                
-        }
+	$colours = array("#00000f","#ff0000","#00ff00","#ffff00","#0000ff");
+	$i = 0;
+	foreach ($json_conf['probejob_partitions'] as $partition)
+	{
+		$series = $series."LINE2:'".$partition."'".$colours[$i].":'".$partition."' ";
+		$i++;
+	}
 
-        
-        //required
-        $rrdtool_graph['title'] = '-- Slurm Time Probe Jobs Spent Pending --';
-        $rrdtool_graph['vertical-label'] = 'seconds';
-        $rrdtool_graph['series'] = $series;
+	//required
+	$rrdtool_graph['title'] = '-- Slurm Time Probe Jobs Spent Pending --';
+	$rrdtool_graph['vertical-label'] = 'seconds';
+	$rrdtool_graph['series'] = $series;
 
-        //      return $rrdtool_graph;
+	//      return $rrdtool_graph;
 }
 ?>
